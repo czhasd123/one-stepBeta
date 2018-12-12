2b32a5e3baabdbcdbf74d743e460ce357e4ed028
ppdb <499185845@qq.com>
2018-12-12 20:20:01 +0800

    Revert "初始项目文件"
    This reverts commit 961e0e849994747c9d5c0e5b67d698ea6490bd5a.

--- OnestepBeta/app/src/main/java/com/example/administrator/tools/HttpCallbackListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.administrator.tools;
-
-public interface HttpCallbackListener {
-
-    void onFinish(String response);
-    void onError(Exception e);
-}
--- OnestepBeta/app/src/main/java/com/example/administrator/tools/userInformation.java
+++
@@ -1,6 +0,0 @@
-package com.example.administrator.tools;
-import com.example.administrator.model.User;
-
-public class userInformation {
-    private static User user ;
-}
